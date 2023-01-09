cb12349a8df64ed493d798cfa9408017389856b6
Xiaozhe Yao <askxzyao@gmail.com>
2023-01-09 12:24:47 +0100

    putting sif to aws

--- together_node/src/constants.py
+++ together_node/src/constants.py
@@ -1,6 +1,6 @@
 MODEL_CONFIG = {
     'opt-6.7b': {
-        "singularity_url": "https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/ft_base_ampere.sif",
+        "singularity_url": "https://together-singularity.s3.us-west-2.amazonaws.com/stablediffusion_general.sif",
         "weights_url":"https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/opt-6.7b-tp1.tar.gz",
         "docker_id": "togethercomputer/fastertransformer",
         "sif_name": "ft_base_ampere.sif",
@@ -9,7 +9,7 @@ MODEL_CONFIG = {
         "startup_command":"/workspace/Port_FasterTransformer/serve.sh"
     },
     'opt-1.3b': {
-        "singularity_url": "https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/ft_base_ampere.sif",
+        "singularity_url": "https://together-singularity.s3.us-west-2.amazonaws.com/stablediffusion_general.sif",
         "weights_url":"https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/opt-1.3b-tp1.tar.gz",
         "worker_model":"opt-1.3b-tp1",
         "docker_id": "togethercomputer/fastertransformer",
