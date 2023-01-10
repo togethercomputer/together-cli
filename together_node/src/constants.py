47c2baa051e2810ddb17a5e8923772a44179bf49
Xiaozhe Yao <askxzyao@gmail.com>
2023-01-10 20:16:13 +0100

    finishing testing on ETH

--- together_node/src/constants.py
+++ together_node/src/constants.py
@@ -1,34 +1,34 @@
 MODEL_CONFIG = {
     'opt-6.7b': {
-        "singularity_url": "https://together-singularity.s3.us-west-2.amazonaws.com/ft_base_ampere.sif",
+        "singularity_url": "https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/ft_base_ampere.sif",
         "weights_url":"https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/opt-6.7b-tp1.tar.gz",
-        "docker_id": "togethercomputer/fastertransformer",
+        "docker_id": "xzyaoi/faster_transformer:ampere",
         "sif_name": "ft_base_ampere.sif",
         "worker_model":"opt-6.7b-tp1",
         "model_type": "gpt",
         "startup_command":"/workspace/Port_FasterTransformer/serve.sh"
     },
     'opt-1.3b': {
-        "singularity_url": "https://together-singularity.s3.us-west-2.amazonaws.com/ft_base_ampere.sif",
+        "singularity_url": "https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/ft_base_ampere.sif",
         "weights_url":"https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/opt-1.3b-tp1.tar.gz",
         "worker_model":"opt-1.3b-tp1",
-        "docker_id": "togethercomputer/fastertransformer",
+        "docker_id": "xzyaoi/faster_transformer:ampere",
         "sif_name": "ft_base_ampere.sif",
         "model_type": "gpt",
         "startup_command":"/workspace/Port_FasterTransformer/serve.sh"
     },
     'stable-diffusion': {
-        "singularity_url": "https://together-singularity.s3.us-west-2.amazonaws.com/stablediffusion_general.sif",
+        "singularity_url": "https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/stablediffusion_general.sif",
         "worker_model":"StableDiffusion",
         "docker_id": "togethercomputer/fastertransformer",
         "sif_name": "stablediffusion_general.sif",
         "startup_command":"/home/user/serve.sh"
     },
     'gpt-jt': {
-        "singularity_url": "https://together-singularity.s3.us-west-2.amazonaws.com/ft_base_ampere.sif",
+        "singularity_url": "https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/ft_base_ampere.sif",
         "weights_url":"https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/opt-1.3b-tp1.tar.gz",
         "worker_model":"GPT-JT-6B-v1-tp1",
-        "docker_id": "togethercomputer/fastertransformer",
+        "docker_id": "xzyaoi/faster_transformer:ampere",
         "sif_name": "ft_base_ampere.sif",
         "model_type": "gpt",
         "startup_command":"/workspace/Port_FasterTransformer/serve.sh"
